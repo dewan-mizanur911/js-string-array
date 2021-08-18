8b1b1794c448e2c1d0917874ec9f8c89ffc51229
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-19 00:34:46 +0600

    argument file added

---
+++ arguments.js
@@ -0,0 +1,19 @@
+function add() {
+    let total = 0;
+    for (const number of arguments) {
+        total = total + number;
+    }
+    return total;
+}
+const numbers = add(3, 5, 6, 94823, 992, 1);
+console.log(numbers);
+
+function concatName() {
+    let fullName = '';
+    for (const name of arguments) {
+        fullName = fullName + name + ' ';
+    }
+    return fullName;
+}
+const name = concatName('rohim', 'korim', 'lala');
+console.log(name);
