5bdee2ed7b472b477000c56a634a8742cd72ee0c
Dewan Mizanur Rahman <dewan.mizanur911@gmail.com>
2021-08-18 22:19:49 +0600

    js array-string

---
+++ case-convert.js
@@ -0,0 +1,8 @@
+const userInput = 'bLackBeLt';
+const savedUser = 'blackBelt';
+if (userInput.toLowerCase() == savedUser.toLowerCase()) {
+    console.log('user exists');
+}
+else {
+    console.log('not matched');
+}
